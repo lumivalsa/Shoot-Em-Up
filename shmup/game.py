0b9365e1db7a3ff3b8d93036a5a6cc3418dce9e3
lumivalsa <lumivalsa@gmail.com>
2022-11-15 16:36:26 +0100

    actualizacion

--- shmup/game.py
+++ shmup/game.py
@@ -1,19 +1,24 @@
 import pygame
 from importlib import resources
 
+
 class Game:
+
+    hero_image_path, hero_image_filename="shmup.assets.img","hero.png"
+    screen_size=(640,480)
+    
     def __init__(self):
         pass
 
     def run(self):
         pygame.init()
 
-        screen= pygame.display.set_mode((640,480),0,32)
+        screen= pygame.display.set_mode(Game.screen_size,0,32)
         pygame.display.set_caption("Hello")
 
-        with resources.path("assets.img","hero.png")as hero_path:
+        with resources.path(Game.hero_image_path, Game.hero_image_filename)as hero_path:
             hero_image= pygame.image.load(hero_path).convert_alpha()
-
+            hero_image= pygame.transform.scale(hero_image, (60, 60))
         running= True 
         while running:
             for event in pygame.event.get():
@@ -25,6 +30,7 @@ class Game:
             x,y=pygame.mouse.get_pos()
             x -= hero_image.get_width()/2
             y -= hero_image.get_height()/2
+
             screen.blit(hero_image,(x,y))
 
 
