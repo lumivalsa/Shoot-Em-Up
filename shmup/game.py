525313d2b80d243654ec5ba0c7a65f8fe81324ad
lumivalsa <lumivalsa@gmail.com>
2022-11-17 08:03:21 +0100

    update
    update of the game doc

--- shmup/game.py
+++ shmup/game.py
@@ -5,6 +5,7 @@ from importlib import resources
 class Game:
 
     hero_image_path, hero_image_filename="shmup.assets.img","hero.png"
+    font_path, font_filename= "shmup.assets.fonts","Sansation.ttf"
     screen_size=(640,480)
     
     def __init__(self):
@@ -15,6 +16,11 @@ class Game:
 
         screen= pygame.display.set_mode(Game.screen_size,0,32)
         pygame.display.set_caption("Hello")
+
+        with resources.path(Game.font_path, Game.font_filename)as font_path:
+            my_font= pygame.font.Font(font_path,20)
+        
+        text_sf= my_font.render("Hello!!", True, (0,0,0),(255,255,255))
 
         with resources.path(Game.hero_image_path, Game.hero_image_filename)as hero_path:
             hero_image= pygame.image.load(hero_path).convert_alpha()
@@ -32,6 +38,7 @@ class Game:
             y -= hero_image.get_height()/2
 
             screen.blit(hero_image,(x,y))
+            screen.blit(text_sf,(0,0))
 
 
             pygame.display.update()
